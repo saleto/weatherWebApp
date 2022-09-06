17b9c5cdf4b6651831164c023ed3dc2252cb363c
WIll <saletomoto@gmail.com>
2022-09-06 21:02:25 +0700

    Added weather icons

--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -28,7 +28,6 @@ export class WeatherComponent implements OnInit {
       this.weatherData = data;
       console.log(this.weatherData);
     });
-    // .subscribe(data => console.log(data));
   }
 
 
